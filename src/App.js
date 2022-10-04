a83aa5de4affb23d4158144dd8191f111bcbf5f7
Mmesoma Saint <mmesomasaint@gmail.com>
2022-10-04 08:42:59 +0100

    Design DOM layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-[100vh] grid place-items-center'>
+      <div className='slider w-full max-w-[800px] h-[350px] relative overflow-hidden rounded-2xl'>
+        <div className='slide w-full max-w-[800px] h-[350px] absolute transition duration-500'>
+          <div className='w-full h-full bg-red-600 flex items-center justify-center text-5xl'>1</div>
+        </div>
+
+        <div className='slide w-full max-w-[800px] h-[350px] absolute transition duration-500'>
+          <div className='w-full h-full bg-green-600 flex items-center justify-center text-5xl'>2</div>
+        </div>
+
+        <div className='slide w-full max-w-[800px] h-[350px] absolute transition duration-500'>
+          <div className='w-full h-full bg-blue-600 flex items-center justify-center text-5xl'>3</div>
+        </div>
+
+        <div className='slide w-full max-w-[800px] h-[350px] absolute transition duration-500'>
+          <div className='w-full h-full bg-purple-600 flex items-center justify-center text-5xl'>4</div>
+        </div>
+
+        <button className='btn btn-next flex justify-center items-center absolute top-[45%] right-[2%] w-[40px] h-[40px] p-[10px] border-none rounded-[50%] z-[10] cursor-pointer bg-[#fff] text-[18px] active:transform active:scale-[1.1]'>
+          {'>'}
+        </button>
+        <button className='btn btn-prev flex justify-center items-center absolute top-[45%] left-[2%] w-[40px] h-[40px] p-[10px] border-none rounded-[50%] z-[10] cursor-pointer bg-[#fff] text-[18px] active:transform active:scale-[1.1]'>
+          {'<'}
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
