dff88e6ba98f210f0baec8872dac0cbbb6fdedfe
Mmesoma Saint <mmesomasaint@gmail.com>
2022-10-04 08:45:41 +0100

    Transition effect function added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,66 @@
+import { useEffect } from 'react'
+
 function App() {
+  useEffect(() => {
+    const loadingTransition = () => {
+      // Select all slides
+      const slides = document.querySelectorAll('.slide')
+
+      // loop through slides and set each slides translateX
+      slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${indx * 100}%)`
+      })
+
+      // select next slide button
+      const nextSlide = document.querySelector('.btn-next')
+
+      // current slide counter
+      let curSlide = 0
+      // maximum number of slides
+      let maxSlide = slides.length - 1
+
+      const next = function () {
+        // check if current slide is the last and reset current slide
+        if (curSlide === maxSlide) {
+          curSlide = 0
+        } else {
+          curSlide++
+        }
+
+        //   move slide by -100%
+        slides.forEach((slide, indx) => {
+          slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`
+        })
+      }
+
+      // add event listener and navigation functionality
+      nextSlide.addEventListener('click', next)
+
+      // select next slide button
+      const prevSlide = document.querySelector('.btn-prev')
+
+      const prev = function () {
+        // check if current slide is the first and reset current slide to last
+        if (curSlide === 0) {
+          curSlide = maxSlide
+        } else {
+          curSlide--
+        }
+
+        // move slide by 100%
+        slides.forEach((slide, indx) => {
+          slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`
+        })
+      }
+
+      // add event listener and navigation functionality
+      prevSlide.addEventListener('click', prev)
+    }
+
+    loadingTransition()
+  }, [])
+
+
   return (
     <div className='h-[100vh] grid place-items-center'>
       <div className='slider w-full max-w-[800px] h-[350px] relative overflow-hidden rounded-2xl'>
